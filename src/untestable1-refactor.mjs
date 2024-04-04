2e9b1aec715036f5b9cfcb9493e8fd807443947e
David Andrea <davejandrea@gmail.com>
2024-04-04 13:30:54 -0300

    make code testable

--- src/untestable1-refactor.mjs
+++ src/untestable1-refactor.mjs
@@ -1,11 +1,21 @@
 const millisPerDay = 24 * 60 * 60 * 1000;
 
 export function daysUntilChristmas(date) {
-  const today = new Date(date.getFullYear(), date.getMonth(), date.getDate());
-  const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
-  if (today.getTime() > christmasDay.getTime()) {
+  if (!date) {
+    date = new Date();
+  }
+
+  if (typeof date === "string") {
+    date = new Date(date)
+  }
+
+  const dateToCheck = new Date(new Date().getFullYear(), date.getMonth(), date.getDate());
+  const christmasDay = new Date(dateToCheck.getFullYear(), 12 - 1, 25);
+
+  if (dateToCheck.getTime() > christmasDay.getTime() + millisPerDay) {
     christmasDay.setFullYear(new Date().getFullYear() + 1);
   }
-  const diffMillis = christmasDay.getTime() - today.getTime();
+
+  const diffMillis = christmasDay.getTime() - dateToCheck.getTime() - millisPerDay;
   return Math.floor(diffMillis / millisPerDay);
 }
