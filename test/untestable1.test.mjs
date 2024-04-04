ce99a18ecd7f53c3685cebcb3cf066577bcacb8b
David Andrea <davejandrea@gmail.com>
2024-04-04 13:31:22 -0300

    make code testable

--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -1,10 +1,18 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
-import { daysUntilChristmas } from "../src/untestable1.mjs";
+import { daysUntilChristmas } from "../src/untestable1-refactor.mjs";
 
 describe("Untestable 1: days until Christmas", () => {
-  test("todo", () => {
-    // TODO: write proper tests
+  const januaryFirst = "2015-01-01";
+  const decemberTwentieth = "2015-12-20";
+
+  test("daysUntilChristmas should return a number", () => {
     expect(daysUntilChristmas()).to.be.a("number");
   });
+
+  test("should return 5", () => {
+    expect(daysUntilChristmas(decemberTwentieth)).toEqual(5);
+  })
+
+
 });
