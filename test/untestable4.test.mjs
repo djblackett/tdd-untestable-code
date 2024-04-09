239104e5717764658dfe3c2725d602cf39694d7a
David Andrea <davejandrea@gmail.com>
2024-04-08 21:42:42 -0300

    basic test setup using mocked  objs

--- test/untestable4.test.mjs
+++ test/untestable4.test.mjs
@@ -1,10 +1,17 @@
 import { afterEach, beforeEach, describe, test } from "vitest";
-import { PasswordService, PostgresUserDao } from "../src/untestable4.mjs";
+import { PostgresUserDao } from "../src/untestable4.mjs";
+import {InMemoryUserDao, MockHasher, PasswordService} from "../src/untestable4-refactor.mjs";
 
 describe("Untestable 4: enterprise application", () => {
+  let userId = 12345;
   let service;
+  let hasher;
+  let users;
+
   beforeEach(() => {
-    service = new PasswordService();
+    users = new InMemoryUserDao()
+    hasher = new MockHasher()
+    service = new PasswordService(users, hasher)
   });
 
   afterEach(() => {
